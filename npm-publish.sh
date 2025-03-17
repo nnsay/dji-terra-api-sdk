d1d3bba41e9f01f8d5fd98a2b6e1ed2376e632b2
Jimmy Wang <jimmy.w@aliyun.com>
2025-03-17 14:30:29 +0800

    ci: publish to npm

---
+++ npm-publish.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# 1. package
+yarn pack
+
+# 2. build
+tag=$(jq -r '.version' package.json)
+tar zxf nnsay-dji-terra-api-sdk-v"$tag".tgz
+
+# 3. publish
+cd package
+npm publish --access public
+
+# 4. clean
+cd ..
+rm -rf *.tgz package
